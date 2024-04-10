b1472e7fb76e945bcbf4df129c340f173b6c5057
Elysia <148540043+elysia090@users.noreply.github.com>
2024-04-10 16:15:06 +0900

    Update C1.DSS-VAE1.py

--- C1.DSS-VAE1.py
+++ C1.DSS-VAE1.py
@@ -1,21 +1,33 @@
 import tensorflow as tf
-import numpy as np
 
-class VAE(tf.keras.Model):
-    def __init__(self, latent_dim):
-        super(VAE, self).__init__()
+class DSSVAE(tf.keras.Model):
+    def __init__(self, input_dim, latent_dim, state_dim):
+        super(DSSVAE, self).__init__()
+        self.input_dim = input_dim
         self.latent_dim = latent_dim
+        self.state_dim = state_dim
+
+        # VAEのエンコーダー
         self.encoder = tf.keras.Sequential([
             tf.keras.layers.InputLayer(input_shape=(input_dim,)),
             tf.keras.layers.Dense(256, activation='relu'),
             tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(latent_dim + latent_dim) # mean and log_var
         ])
+
+        # VAEのデコーダー
         self.decoder = tf.keras.Sequential([
             tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
             tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(256, activation='relu'),
             tf.keras.layers.Dense(input_dim)
+        ])
+
+        # 状態空間モデルの推定器
+        self.ssm_estimator = tf.keras.Sequential([
+            tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(state_dim + input_dim) # state and observation
         ])
 
     def encode(self, x):
@@ -29,57 +41,52 @@ class VAE(tf.keras.Model):
     def decode(self, z):
         return self.decoder(z)
 
+    def ssm_estimate(self, z):
+        return tf.split(self.ssm_estimator(z), num_or_size_splits=[self.state_dim, self.input_dim], axis=1)
+
     def call(self, x):
         mean, log_var = self.encode(x)
         z = self.reparameterize(mean, log_var)
         x_reconstructed = self.decode(z)
-        return x_reconstructed, mean, log_var
+        state, observation = self.ssm_estimate(z)
+        return x_reconstructed, mean, log_var, state, observation
 
-def vae_loss(x, x_reconstructed, mean, log_var):
+def dssvae_loss(x, x_reconstructed, mean, log_var, observation):
     reconstruction_loss = tf.reduce_mean(tf.square(x - x_reconstructed))
     kl_loss = -0.5 * tf.reduce_mean(1 + log_var - tf.square(mean) - tf.exp(log_var))
-    return reconstruction_loss + kl_loss
+    observation_loss = tf.reduce_mean(tf.square(x - observation))
+    return reconstruction_loss + kl_loss + observation_loss
 
-class StateSpaceModel(tf.keras.Model):
-    def __init__(self, latent_dim, state_dim):
-        super(StateSpaceModel, self).__init__()
-        self.latent_dim = latent_dim
-        self.state_dim = state_dim
-        self.transition = tf.keras.Sequential([
-            tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-            tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(state_dim)
-        ])
-        self.observation = tf.keras.Sequential([
-            tf.keras.layers.InputLayer(input_shape=(state_dim,)),
-            tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(input_dim)
-        ])
+# データの前処理とモデルのインスタンス化
+input_dim = 784  # 入力次元数
+latent_dim = 64  # 潜在空間の次元数
+state_dim = 32   # 状態空間の次元数
 
-    def call(self, z):
-        state = self.transition(z)
-        observation = self.observation(state)
-        return state, observation
+# モデルのインスタンス化
+dss_vae = DSSVAE(input_dim, latent_dim, state_dim)
 
-def train_step(x, vae, ssm, vae_optimizer, ssm_optimizer):
-    with tf.GradientTape() as tape:
-        x_reconstructed, mean, log_var = vae(x)
-        reconstruction_loss = vae_loss(x, x_reconstructed, mean, log_var)
-    gradients = tape.gradient(reconstruction_loss, vae.trainable_variables)
-    vae_optimizer.apply_gradients(zip(gradients, vae.trainable_variables))
+# オプティマイザの設定
+optimizer = tf.keras.optimizers.Adam()
 
-    with tf.GradientTape() as tape:
-        _, _, log_var = vae(x)
-        kl_loss = -0.5 * tf.reduce_mean(1 + log_var - tf.square(mean) - tf.exp(log_var))
-    gradients = tape.gradient(kl_loss, vae.trainable_variables)
-    vae_optimizer.apply_gradients(zip(gradients, vae.trainable_variables))
+# 訓練ループ
+epochs = 10
+batch_size = 128
 
-    with tf.GradientTape() as tape:
-        _, _, log_var = vae(x)
-        kl_loss = -0.5 * tf.reduce_mean(1 + log_var - tf.square(mean) - tf.exp(log_var))
-        state, observation = ssm(mean)
-        observation_loss = tf.reduce_mean(tf.square(x - observation))
-        total_loss = reconstruction_loss + kl_loss + observation_loss
-    gradients = tape.gradient(total_loss, vae.trainable_variables + ssm.trainable_variables)
-    vae_optimizer.apply_gradients(zip(gradients[:len(vae.trainable_variables)], vae.trainable_variables))
-    ssm_optimizer.apply_gradients(zip(gradients[len(vae.trainable_variables):], ssm.trainable_variables))
+for epoch in range(epochs):
+    for step in range(num_batches):
+        x_batch = # バッチデータの取得
+        with tf.GradientTape() as tape:
+            x_reconstructed, mean, log_var, _, observation = dss_vae(x_batch)
+            loss = dssvae_loss(x_batch, x_reconstructed, mean, log_var, observation)
+        gradients = tape.gradient(loss, dss_vae.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, dss_vae.trainable_variables))
+
+# 推論
+def inference(x_test, dss_vae):
+    x_reconstructed, _, _, state, observation = dss_vae(x_test)
+    return observation
+
+# テストデータに対する推論の実行
+x_test = # テストデータの準備
+x_pred = inference(x_test, dss_vae)
+
