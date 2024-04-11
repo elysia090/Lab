1a7d70924699e549a4dca00f4c0827339be7a1d6
Elysia <148540043+elysia090@users.noreply.github.com>
2024-04-11 11:26:13 +0900

    Update C4.メモ.py

--- C4.メモ.py
+++ C4.メモ.py
@@ -1,33 +1,101 @@
-import re
-import polars as pl
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, cohen_kappa_score
-from sklearn.model_selection import StratifiedKFold
+import gc
 import lightgbm as lgb
+import nltk
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import re
+import random
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV, StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier, GradientBoostingClassifier, BaggingClassifier
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB
+from sklearn.neural_network import MLPClassifier
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from imblearn.ensemble import BalancedBaggingClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, cohen_kappa_score
+from lightgbm import log_evaluation, early_stopping
+import polars as pl
 
-# テキストの前処理
-def preprocess_text(text):
-    text = text.lower()
-    text = remove_html_tags(text)
-    text = re.sub("@\w+", '', text) # '@'で始まる文字列を削除
-    text = re.sub("'\d+", '', text) # 数字を削除
-    text = re.sub("\d+", '', text)  # 数字を削除
-    text = re.sub("http\w+", '', text)  # URLを削除
-    text = re.sub(r"\s+", " ", text)  # 連続した空白を1つの空白に置換
-    text = re.sub(r"\.+", ".", text)  # 連続したピリオドを1つに置換
-    text = re.sub(r"\,+", ",", text)  # 連続したカンマを1つに置換
-    text = text.strip()  # 先頭と末尾の空白を削除
+nltk.download('wordnet')
+
+# Define a function for preprocessing data
+def dataPreprocessing(text):
+    text = text.lower()  # Convert text to lowercase
+    text = re.sub(r'[^\w\s]', '', text)  # Remove punctuation
+    text = re.sub(r'\d+', '', text)  # Remove numbers
+    text = re.sub(r'\s+', ' ', text).strip()  # Remove extra whitespaces
     return text
 
-# HTMLタグの削除
-def remove_html_tags(text):
-    html_pattern = re.compile(r'<.*?>')
-    return html_pattern.sub(r'', text)
+# Define the number of splits for cross-validation
+n_splits = 5
 
-# TfidfVectorizerのパラメータ設定
+# Define the path to the data
+PATH = "/kaggle/input/learning-agency-lab-automated-essay-scoring-2/"
+
+# Read the training and testing data
+columns = [(pl.col("full_text").str.split(by="\n\n").alias("paragraph")),]
+train = pl.read_csv(PATH + "train.csv").with_columns(columns)
+test = pl.read_csv(PATH + "test.csv").with_columns(columns)
+
+# paragraph features
+def Paragraph_Preprocess(tmp):
+    tmp = tmp.explode('paragraph')  # Expand the paragraph list into several lines of data
+    tmp = tmp.with_columns(pl.col('paragraph').map_elements(dataPreprocessing))
+    tmp = tmp.with_columns(pl.col('paragraph').map_elements(lambda x: len(x)).alias("paragraph_len"))
+    tmp = tmp.with_columns(pl.col('paragraph').map_elements(lambda x: len(x.split('.'))).alias("paragraph_sentence_cnt"),
+                    pl.col('paragraph').map_elements(lambda x: len(x.split(' '))).alias("paragraph_word_cnt"),)
+    return tmp
+
+paragraph_fea = ['paragraph_len','paragraph_sentence_cnt','paragraph_word_cnt']
+def Paragraph_Eng(train_tmp):
+    aggs = [
+        *[pl.col('paragraph').filter(pl.col('paragraph_len') >= i).count().alias(f"paragraph_{i}_cnt") for i in [50,75,100,125,150,175,200,250,300,350,400,500,600,700]],
+        *[pl.col('paragraph').filter(pl.col('paragraph_len') <= i).count().alias(f"paragraph_{i}_cnt") for i in [25,49]], 
+        *[pl.col(fea).max().alias(f"{fea}_max") for fea in paragraph_fea],
+        *[pl.col(fea).mean().alias(f"{fea}_mean") for fea in paragraph_fea],
+        *[pl.col(fea).min().alias(f"{fea}_min") for fea in paragraph_fea],
+        *[pl.col(fea).first().alias(f"{fea}_first") for fea in paragraph_fea],
+        *[pl.col(fea).last().alias(f"{fea}_last") for fea in paragraph_fea],
+        ]
+    df = train_tmp.group_by(['essay_id'], maintain_order=True).agg(aggs).sort("essay_id")
+    df = df.to_pandas()
+    return df
+
+tmp = Paragraph_Preprocess(train)
+train_feats = Paragraph_Eng(tmp)
+train_feats['score'] = train['score']
+
+# word features
+def Word_Preprocess(tmp):
+    tmp = tmp.with_columns(pl.col('full_text').map_elements(dataPreprocessing).str.split(by=" ").alias("word"))
+    tmp = tmp.explode('word')
+    tmp = tmp.with_columns(pl.col('word').map_elements(lambda x: len(x)).alias("word_len"))
+    tmp = tmp.filter(pl.col('word_len')!=0)
+    return tmp
+
+def Word_Eng(train_tmp):
+    aggs = [
+        *[pl.col('word').filter(pl.col('word_len') >= i+1).count().alias(f"word_{i+1}_cnt") for i in range(15) ], 
+        pl.col('word_len').max().alias(f"word_len_max"),
+        pl.col('word_len').mean().alias(f"word_len_mean"),
+        pl.col('word_len').std().alias(f"word_len_std"),
+        pl.col('word_len').quantile(0.25).alias(f"word_len_q1"),
+        pl.col('word_len').quantile(0.50).alias(f"word_len_q2"),
+        pl.col('word_len').quantile(0.75).alias(f"word_len_q3"),
+        ]
+    df = train_tmp.group_by(['essay_id'], maintain_order=True).agg(aggs).sort("essay_id")
+    df = df.to_pandas()
+    return df
+
+tmp = Word_Preprocess(train)
+train_feats = train_feats.merge(Word_Eng(tmp), on='essay_id', how='left')
+
+# TF-IDF features
 vectorizer = TfidfVectorizer(
             tokenizer=lambda x: x,
             preprocessor=lambda x: x,
@@ -40,87 +108,21 @@ vectorizer = TfidfVectorizer(
             sublinear_tf=True,
 )
 
-# TfidfVectorizerを使って特徴量を生成し、データに統合
-def generate_and_merge_tfidf_features(data, vectorizer):
-    train_tfid = vectorizer.fit_transform([preprocess_text(text) for text in data['full_text']])
-    dense_matrix = train_tfid.toarray()
-    df = pd.DataFrame(dense_matrix, columns=[f'tfid_{i}' for i in range(len(dense_matrix[0]))])
-    df['essay_id'] = data['essay_id']
-    return data.merge(df, on='essay_id', how='left')
+train_tfid = vectorizer.fit_transform([i for i in train['full_text']])
+dense_matrix = train_tfid.toarray()
+df = pd.DataFrame(dense_matrix)
+tfid_columns = [f'tfid_{i}' for i in range(len(df.columns))]
+df.columns = tfid_columns
+df['essay_id'] = train_feats['essay_id']
+train_feats = train_feats.merge(df, on='essay_id', how='left')
 
-# 特徴量の生成
-def generate_features(data):
-    data = preprocess_paragraphs(data)
-    data = preprocess_sentences(data)
-    data = preprocess_words(data)
-    data = generate_and_merge_tfidf_features(data, vectorizer)
-    return data
+# Define the objective function for Quadratic Weighted Kappa
+def quadratic_weighted_kappa(y_true, y_pred):
+    y_true = y_true + a
+    y_pred = (y_pred + a).clip(1, 6).round()
+    qwk = cohen_kappa_score(y_true, y_pred, weights="quadratic")
+    return 'QWK', qwk, True
 
-# 学習および評価
-def train_and_evaluate(X_train, y_train, X_test):
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
-    f1_scores = []
-    kappa_scores = []
-    models = []
-
-    for i, (train_index, test_index) in enumerate(skf.split(X_train, y_train), 1):
-        print('fold', i)
-        X_train_fold, X_test_fold = X_train[train_index], X_train[test_index]
-        y_train_fold, y_test_fold = y_train[train_index], y_train[test_index]
-
-        model = lgb.LGBMRegressor(
-                    objective=qwk_obj,
-                    metrics='None',
-                    learning_rate=0.1,
-                    max_depth=5,
-                    num_leaves=10,
-                    colsample_bytree=0.5,
-                    reg_alpha=0.1,
-                    reg_lambda=0.8,
-                    n_estimators=1024,
-                    random_state=42,
-                    extra_trees=True,
-                    class_weight='balanced',
-                    verbosity=-1
-                )
-        
-        predictor = model.fit(
-                        X_train_fold,
-                        y_train_fold,
-                        eval_names=['train', 'valid'],
-                        eval_set=[(X_train_fold, y_train_fold), (X_test_fold, y_test_fold)],
-                        eval_metric=quadratic_weighted_kappa,
-                        callbacks=[log_evaluation(period=25), early_stopping(stopping_rounds=75, first_metric_only=True)]
-                    )
-        
-        models.append(predictor)
-        
-        predictions_fold = predictor.predict(X_test_fold)
-        predictions_fold = (predictions_fold + a).clip(1, 6).round()
-        
-        f1_fold = f1_score(y_test_fold, predictions_fold, average='weighted')
-        f1_scores.append(f1_fold)
-        
-        kappa_fold = cohen_kappa_score(y_test_fold, predictions_fold, weights='quadratic')
-        kappa_scores.append(kappa_fold)
-        
-        cm = confusion_matrix(y_test_fold, predictions_fold, labels=[x for x in range(1, 7)])
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[x for x in range(1, 7)])
-        disp.plot()
-        plt.show()
-        
-        print(f'F1 score across fold: {f1_fold}')
-        print(f'Cohen kappa score across fold: {kappa_fold}')
-
-    mean_f1_score = np.mean(f1_scores)
-    mean_kappa_score = np.mean(kappa_scores)
-    
-    print(f'Mean F1 score across 5 folds: {mean_f1_score}')
-    print(f'Mean Cohen kappa score across 5 folds: {mean_kappa_score}')
-    
-    return models
-
-# quadratic weighted kappaの目的関数
 def qwk_obj(y_true, y_pred):
     labels = y_true + a
     preds = y_pred + a
@@ -133,27 +135,132 @@ def qwk_obj(y_true, y_pred):
     hess = np.ones(len(labels))
     return grad, hess
 
-# データ読み込みと特徴量生成
-train_data = pl.read_csv("/kaggle/input/learning-agency-lab-automated-essay-scoring-2/train.csv")
-test_data = pl.read_csv("/kaggle/input/learning-agency-lab-automated-essay-scoring-2/test.csv")
-train_feats = generate_features(train_data)
-test_feats = generate_features(test_data)
-feature_names = [col for col in train_feats.columns if col not in ['essay_id', 'score']]
+a = 2.948
+b = 1.092
 
-# モデルの学習および評価
-X_train = train_feats[feature_names].astype(np.float32).values
-y_train = train_feats['score'].astype(np.float32).values - a
-models = train_and_evaluate(X_train, y_train)
+# Converting the 'text' column to string type and assigning to X
+X = train_feats[feature_names].astype(np.float32).values
 
-# テストデータに対する予測
-X_test = test_feats[feature_names].astype(np.float32).values
-probabilities = [model.predict(X_test) + a for model in models]
+# Converting the 'score' column to integer type and assigning to y
+y_split = train_feats['score'].astype(int).values
+y = train_feats['score'].astype(np.float32).values-a
+
+# Initialize StratifiedKFold with the specified number of splits
+skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=0)
+
+# Lists to store scores
+f1_scores = []
+kappa_scores = []
+models = []
+predictions = []
+callbacks = [log_evaluation(period=25), early_stopping(stopping_rounds=75,first_metric_only=True)]
+
+# Loop through each fold of the cross-validation
+i=1
+for train_index, test_index in skf.split(X, y_split):
+    # Split the data into training and testing sets for this fold
+    print('fold',i)
+    X_train_fold, X_test_fold = X[train_index], X[test_index]
+    y_train_fold, y_test_fold, y_test_fold_int = y[train_index], y[test_index], y_split[test_index]
+    
+    model = lgb.LGBMRegressor(
+                objective = qwk_obj,
+                metrics = 'None',
+                learning_rate = 0.1,
+                max_depth = 5,
+                num_leaves = 10,
+                colsample_bytree=0.5,
+                reg_alpha = 0.1,
+                reg_lambda = 0.8,
+                n_estimators=1024,
+                random_state=42,
+                extra_trees=True,
+                class_weight='balanced',
+                verbosity = - 1)
+
+    # Fit the model on the training data for this fold
+    predictor = model.fit(X_train_fold,
+                          y_train_fold,
+                          eval_names=['train', 'valid'],
+                          eval_set=[(X_train_fold, y_train_fold), (X_test_fold, y_test_fold)],
+                          eval_metric=quadratic_weighted_kappa,
+                          callbacks=callbacks,)
+    models.append(predictor)
+    
+    # Make predictions on the test data for this fold
+    predictions_fold = predictor.predict(X_test_fold)
+    predictions_fold = predictions_fold + a
+    predictions_fold = predictions_fold.clip(1, 6).round()
+    predictions.append(predictions_fold)
+    
+    # Calculate and store the F1 score for this fold
+    f1_fold = f1_score(y_test_fold_int, predictions_fold, average='weighted')
+    f1_scores.append(f1_fold)
+    
+    # Calculate and store the Cohen's kappa score for this fold
+    kappa_fold = cohen_kappa_score(y_test_fold_int, predictions_fold, weights='quadratic')
+    kappa_scores.append(kappa_fold)
+    
+    # Calculating the confusion matrix
+    cm = confusion_matrix(y_test_fold_int, predictions_fold, labels=[x for x in range(1,7)])
+
+    # Displaying the confusion matrix
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                                  display_labels=[x for x in range(1,7)])
+    disp.plot()
+    plt.show()
+    print(f'F1 score across fold: {f1_fold}')
+    print(f'Cohen kappa score across fold: {kappa_fold}')
+    i+=1
+
+# Calculate the mean scores across all folds
+mean_f1_score = np.mean(f1_scores)
+mean_kappa_score = np.mean(kappa_scores)
+
+# Print the mean scores
+print(f'Mean F1 score across {n_splits} folds: {mean_f1_score}')
+print(f'Mean Cohen kappa score across {n_splits} folds: {mean_kappa_score}')
+
+# Paragraph
+tmp = Paragraph_Preprocess(test)
+test_feats = Paragraph_Eng(tmp)
+
+# Sentence
+tmp = Sentence_Preprocess(test)
+test_feats = test_feats.merge(Sentence_Eng(tmp), on='essay_id', how='left')
+
+# Word
+tmp = Word_Preprocess(test)
+test_feats = test_feats.merge(Word_Eng(tmp), on='essay_id', how='left')
+
+# TF-IDF
+test_tfid = vectorizer.transform([i for i in test['full_text']])
+dense_matrix = test_tfid.toarray()
+df = pd.DataFrame(dense_matrix)
+tfid_columns = [f'tfid_{i}' for i in range(len(df.columns))]
+df.columns = tfid_columns
+df['essay_id'] = test_feats['essay_id']
+test_feats = test_feats.merge(df, on='essay_id', how='left')
+
+# Features number
+feature_names = list(filter(lambda x: x not in ['essay_id','score'], test_feats.columns))
+print('Features number: ', len(feature_names))
+test_feats.head(3)
+
+probabilities = []
+for model in models:
+    proba = model.predict(test_feats[feature_names]) + a
+    probabilities.append(proba)
+
+# Compute the average probabilities across all models
 predictions = np.mean(probabilities, axis=0)
 predictions = np.round(predictions.clip(1, 6))
 
-# 提出用ファイルの作成
-submission = pd.read_csv("/kaggle/input/learning-agency-lab-automated-essay-scoring-2/sample_submission.csv")
-submission['score'] = predictions.astype(int)
+# Print the predictions
+print(predictions)
+
+submission = pd.read_csv("/kaggle/output/learning-agency-lab-automated-essay-scoring-2/sample_submission.csv")
+submission['score'] = predictions
+submission['score'] = submission['score'].astype(int)
 submission.to_csv("submission.csv", index=None)
 display(submission.head())
-
